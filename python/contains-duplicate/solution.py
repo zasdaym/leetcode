22f9cbc5eb5f3e2410c3ca8b9b0d2cbcc9069416
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-04-25 17:35:39 +0700

    Add Python Contains Duplicate

---
+++ python/contains-duplicate/solution.py
@@ -0,0 +1,9 @@
+from typing import List, Set
+
+def contains_duplicate(nums: List[int]) -> bool:
+    seen: Set[int] = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
