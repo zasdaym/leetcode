6d3d798e160a2dcb8cc1b65fabdc4c1f4b325852
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-04-29 22:32:11 +0700

    Update Go Binary Search

--- golang/binary-search/solution.go
+++ golang/binary-search/solution.go
@@ -1,13 +1,10 @@
 package binarysearch
 
-import "fmt"
-
 func binarySearch(nums []int, target int) int {
 	left, right := 0, len(nums)-1
 
 	for left <= right {
 		middle := (left + right) / 2
-		fmt.Println(nums[middle])
 		if nums[middle] == target {
 			return middle
 		} else if nums[middle] < target {
