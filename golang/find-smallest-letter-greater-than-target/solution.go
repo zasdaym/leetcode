649e764c5e18ec280196006ec92bc8978b43317c
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-05-01 22:10:58 +0700

    Update Go Find Smallest Letter Greater Than Target

--- golang/find-smallest-letter-greater-than-target/solution.go
+++ golang/find-smallest-letter-greater-than-target/solution.go
@@ -4,7 +4,7 @@ func nextGreatestLetter(letters []byte, target byte) byte {
 	left, right := 0, len(letters)
 
 	for left < right {
-		middle := left + (right-left)/2
+		middle := (left + right) / 2
 		if letters[middle] <= target {
 			left = middle + 1
 		} else {
